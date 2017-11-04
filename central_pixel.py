62404cd29eb5e78784ae9f0884b9a244f7d82da6
Andrey <timebird@yandex.ru>
2017-11-05 01:40:36 +0300

    comment added

--- central_pixel.py
+++ central_pixel.py
@@ -6,7 +6,7 @@ width = img.shape[0]  #ширина фото в пикселях
 height = img.shape[1] #высота
 
 
-#Находим центральный пиксель в изображении:
+#Находим центральный пиксель в изображении и меняем его цвет на зеленый:
 central_pixel_width = int((width - 1) / 2)
 print(central_pixel_width)
 central_pixel_height = int((height - 1) / 2)
